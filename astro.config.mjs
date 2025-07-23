f90d215f3b978f14f1ad862d86e8413a5eaaa57f
flexagoon <code@fxgn.dev>
2025-07-23 20:25:12 +0300

    Revert 1 commits
    240709b 'refactor: don't use an env var for site basename'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -9,6 +9,12 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  env: {
+    schema: {
+      SITE_URL: envField.string({context: "client", access: "public"})
+    }
   },
 
   adapter: cloudflare()
